ef476984087fa5268ec9734bcc74e97eb0d2800b
Paul Nickerson <paul.nickerson@grooveshark.com>
2013-05-27 19:03:03 -0400

    starting from scratch with go

---
+++ consumer.go
@@ -0,0 +1,5 @@
+package consumer
+
+func hello_world() {
+    return "foobar"
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    /*"consumer"*/
+)
+
+func main() {
+    fmt.Println(consumer.hello_world())
+}
