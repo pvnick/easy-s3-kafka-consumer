59d7a26dd09130cb05ef418294de6a3945763510
Paul Nickerson <paul.nickerson@grooveshark.com>
2013-05-27 21:30:50 -0400

    testing and playing around

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,5 +1,16 @@
 package consumer
+/*
+type reader struct {
+    timeout int64
+    prev    int64
+    ival    int64
+    times   []liverec
+    self    *net.UDPAddr
+    shun    chan<- string
+}
 
+func (lv *liveness) mark(a net.Addr, t int64) {
+*/
 func Hello_world() string {
     return "foobar"
 }
